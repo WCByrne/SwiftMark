bc6ab596477fb68a01af2f1b6a649ad0b8f4fb51
Wes Byrne <wesburn@me.com>
2018-10-26 10:58:45 -0700

    Initial Commit

---
+++ SwiftMark/SwiftMark.h
@@ -0,0 +1,19 @@
+//
+//  SwiftMark.h
+//  SwiftMark
+//
+//  Created by Wesley Byrne on 10/26/18.
+//  Copyright © 2018 The Noun Project. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftMark.
+FOUNDATION_EXPORT double SwiftMarkVersionNumber;
+
+//! Project version string for SwiftMark.
+FOUNDATION_EXPORT const unsigned char SwiftMarkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftMark/PublicHeader.h>
+
+
