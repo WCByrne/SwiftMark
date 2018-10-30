91017b254b01357ee26d00b404b3851196b56334
Wes Byrne <wesburn@me.com>
2018-10-30 10:04:02 -0700

    Updated docs, add html stub, Reorg files

---
+++ SwiftMark/Rendering/HTML.swift
@@ -0,0 +1,19 @@
+//
+//  HTML.swift
+//  SwiftMark
+//
+//  Created by Wesley Byrne on 10/30/18.
+//  Copyright © 2018 The Noun Project. All rights reserved.
+//
+
+import Foundation
+
+extension Node {
+    /// Render the node and it's children as HTML
+    ///
+    /// - Returns: A HTML string representing the node
+    func html() -> String {
+        assertionFailure("html is not yet impemented")
+        return ""
+    }
+}
